ca5353eec14ff255e3a1dba622c80c3b1843eae6
eikhyeonchoi <eikhyeon.choi@gmail.com>
2019-05-09 14:47:54 +0900

    second team

---
+++ src/main/webapp/html/faq/index.js
@@ -0,0 +1,36 @@
+var tbody = $('tbody'),
+    templateSrc = $('#tr-template').html();
+var trGenerator = Handlebars.compile(templateSrc);
+
+($.getJSON('/bitcamp-team-project/app/json/faq/list', (obj) => {
+  console.log(obj);
+  $(trGenerator(obj)).appendTo(tbody);
+  $(document.body).trigger('loaded-list');
+}));
+
+
+$(document.body).bind('loaded-list', () => {
+  $('.faq-a-class').click((e) => {
+    e.preventDefault();
+    window.location.href = 'view.html?no=' + 
+      $(e.target).attr('data-no');
+  });
+});
+
+/*
+(function () {
+  var xhr = new XMLHttpRequest()
+  
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4 || xhr.status != 200)
+      return;
+    $(trGenerator(JSON.parse(xhr.responseText))).appendTo(tbody);
+    
+    console.log(JSON.parse(xhr.responseText));
+  };
+  
+  xhr.open('GET', '/bitcamp-team-project/app/json/faq/list', true)
+  xhr.send()
+  
+})();
+*/
