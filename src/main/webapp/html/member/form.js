0bdc500fa005565caa436db67d13e59fc907481f
Leedaegu <dencw6@naver.com>
2019-05-14 09:35:36 +0900

    .

--- src/main/webapp/html/member/form.js
+++ src/main/webapp/html/member/form.js
@@ -9,22 +9,28 @@ if (who == "nomal") {
   $('h1').html("기업회원 가입");
 }
 
-$('#add-btn').click(function() {
-  $.post('../../app/json/member/add', {
-    name: $('#name').val(),
-    nickName: $('#nickName').val(),
-    email: $('#email').val(),
-    password: $('#password').val(),
-    tel: $('#tel').val(),
-    type: 1
-  },
-  function(data) {
-    headers: ("Content-Type", "application/x-www-form-urlencoded");
-  if (data.status == 'success') {
-    location.href = "index.html";
-  }
-  }).fail(function(data) {
-    alert('등록 실패 입니다.\n' +  data.message);
+if (who == "nomal") {
+  
+  $('#add-btn').click(function(e) {
+    
+    $.post('../../app/json/member/add', {
+      name: $('#name').val(),
+      nickName: $('#nickName').val(),
+      email: $('#email').val(),
+      password: $('#password').val(),
+      tel: $('#tel').val(),
+      type: "일반회원"
+    },
+    function(data) {
+      headers: ("Content-Type", "application/x-www-form-urlencoded");
+    if (data.status == 'success') {
+      location.href = "index.html";
+    }
+    }).fail(function(data) {
+      alert('등록 실패 입니다.\n' +  data.message);
+    });
   })
-})
+} else {
+  
+}
 
