a1427b60bf2515349e2538cf4af16a1a2333dbbd
bitcamp <seungwan.woo94@gmail.com>
2019-05-14 17:33:44 +0900

    .

--- src/main/java/bitcamp/team/web/json/SatisfyController.java
+++ src/main/java/bitcamp/team/web/json/SatisfyController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import bitcamp.team.domain.Satisfy;
 import bitcamp.team.service.SatisfyService;
 
 @RestController("json/SatisfyController")
