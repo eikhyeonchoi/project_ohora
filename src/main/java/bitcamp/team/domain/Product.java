6717ad671cee5f7c48806367add53c38ed3a5a54
jisung12 <01054153608a@gmail.com>
2019-05-14 13:45:32 +0900

    .

--- src/main/java/bitcamp/team/domain/Product.java
+++ src/main/java/bitcamp/team/domain/Product.java
@@ -9,19 +9,6 @@ public class Product {
   private Manufacturer manufacturer;
   private ProductSmallCategory productSmallCategory;
   
-  
-  public Manufacturer getManufacturer() {
-    return manufacturer;
-  }
-  public void setManufacturer(Manufacturer manufacturer) {
-    this.manufacturer = manufacturer;
-  }
-  public ProductSmallCategory getProductSmallCategory() {
-    return productSmallCategory;
-  }
-  public void setProductSmallCategory(ProductSmallCategory productSmallCategory) {
-    this.productSmallCategory = productSmallCategory;
-  }
   public int getNo() {
     return no;
   }
@@ -46,35 +33,19 @@ public class Product {
   public void setName(String name) {
     this.name = name;
   }
+  public Manufacturer getManufacturer() {
+    return manufacturer;
+  }
+  public void setManufacturer(Manufacturer manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+  public ProductSmallCategory getProductSmallCategory() {
+    return productSmallCategory;
+  }
+  public void setProductSmallCategory(ProductSmallCategory productSmallCategory) {
+    this.productSmallCategory = productSmallCategory;
+  }
   
-  private int sctgNo;
-  private int manufacNo;
-  private String name;
-  
-  public int getNo() {
-    return no;
-  }
-  public void setNo(int no) {
-    this.no = no;
-  }
-  public int getSctgNo() {
-    return sctgNo;
-  }
-  public void setSctgNo(int sctgNo) {
-    this.sctgNo = sctgNo;
-  }
-  public int getManufacNo() {
-    return manufacNo;
-  }
-  public void setManufacNo(int manufacNo) {
-    this.manufacNo = manufacNo;
-  }
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
   
   
 }
--- src/main/webapp/html/satisfy/add.js
+++ src/main/webapp/html/satisfy/add.js
@@ -1,88 +1,29 @@
-var select = $('#faq-question-category-option'),
-templateSrc = $('#select-template').html();
-var trGenerator = Handlebars.compile(templateSrc);
+var addNo = location.href.split('?')[1].split('=')[1];  
+console.log(addNo);
 
-var param = location.href.split('?')[1]; // no=10
+document.getElementById('product-no').value = addNo;
 
-//파라미터가 있을 경우 detail, update, delete
-if(param){ 
-  $('.faq-add-item').hide();
-  
-  // 카테고리목록을 받아오기 위한 쿼리문 ==> handlebars에서 사용함
-  ($.getJSON('/bitcamp-team-project/app/json/faq/list', (obj) => {
-    $(trGenerator(obj)).appendTo(select);
-    $(document.body).trigger('loaded-updateForm');
-  }));
+  // ($('#product-no').css('dispalay', 'none'));
 
-  // console.log(param);
-  $.getJSON('/bitcamp-team-project/app/json/faq/detail?no=' + param.split('=')[1], function(obj) { // 10
-    console.log(obj);
-    $('#faq-no').val(obj.faq.no);
-    $('#faq-title').val(obj.faq.title);
-    $('#faq-contents').val(obj.faq.contents);
-  });
-
-//파라미터가 없을 경우 add form으로 사용한다
-} else {
-  $('.faq-view-item').hide();
-  $('h1').html('자주묻는 질문 - add');
-  
-  // 카테고리목록을 받아오기 위한 쿼리문 ==> handlebars에서 사용함
-  ($.getJSON('/bitcamp-team-project/app/json/faq/list', (obj) => {
-    // console.log(obj);
-    $(trGenerator(obj)).appendTo(select);
-    $(document.body).trigger('loaded-addForm');
-  }));
-}
-
-// 데이터가 로드되고 난 후 add버튼에 리스너추가
-$(document.body).bind('loaded-addForm', () => {
-  $('#faq-add-btn').click(() =>{
-    // console.log($('#faqCtg-no option:selected').val());
-    $.post('/bitcamp-team-project/app/json/faq/add',{
-      title: $('#faq-title').val(),
-      contents: $('#faq-contents').val(),
-      qcNo: $('#faqCtg-no option:selected').val()
+// 등록버튼 클릭시
+  $('#satisfy-add-btn').click(() => {
+    $.post('/bitcamp-team-project/app/json/satisfy/add',{
+      pdNo: $('#product-no').val(),
+      mNo: $('#member-no').val(),
+      level: $('#satisfy-level').val(),
+      understand: $('#satisfy-understand').val(),
+      design: $('#satisfy-design').val(),
+      asStf: $('#satisfy-asStf').val(),
+      useful: $('#satisfy-useful').val(),
+      priceStf: $('#satisfy-priceStf').val(),
+      
     }, function(data) {
       headers: ("Content-Type", "application/x-www-form-urlencoded");
     if(data.status == 'success'){
       location.href='index.html';
-    } else alert("필수 입력값을 입력하지 않았습니다");
-    }) // post
-  }); // click
-}) // bind
-
-
-
-$(document.body).bind('loaded-updateForm', () => {
-  // update
-  $('#faq-update-btn').click(() =>{
-    $.post('/bitcamp-team-project/app/json/faq/update', {
-      no: param.split('=')[1],
-      title: $('#faq-title').val(),
-      contents: $('#faq-contents').val(),
-      qcNo: $('#faqCtg-no option:selected').val()
-    }, function(data) {
-      headers: ("Content-Type", "application/x-www-form-urlencoded");
-      console.log(data);
-      if(data.status == 'success'){
-        location.href='index.html';
-      } else alert("수정 실패\n" + data.message);
-    }) // post
-  }); // click
-  
-  // delete
-  $('#faq-delete-btn').click(() => {
-    $.get('/bitcamp-team-project/app/json/faq/delete?no=' + param.split('=')[1], function(obj){
-      console.log(obj);
-      if(obj.status == 'success'){
-        location.href='index.html';
-      } else alert('삭제 실패\n' +  obj.message) 
-    }) // get
-  }); // click
-  
-  
-}) // bind
+    } else alert("0 ~ 5 사이 번호 입력");
+    })
+  }); 
 
 
 
