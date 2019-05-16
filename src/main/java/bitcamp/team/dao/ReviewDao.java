317fce29fb6fe91760d5f2e501dc66a59253200a
jisung12 <01054153608a@gmail.com>
2019-05-16 18:12:11 +0900

    3

--- src/main/java/bitcamp/team/dao/ReviewDao.java
+++ src/main/java/bitcamp/team/dao/ReviewDao.java
@@ -1,11 +1,13 @@
 package bitcamp.team.dao;
 
 import java.util.List;
-import bitcamp.team.domain.Faq;
 import bitcamp.team.domain.Review;
 
 public interface ReviewDao {
   List<Review> findAll();
   List<Review> findByNo(int no);
   Review findByNo2(int no);
+  
+  int delete(int no);
+  int update(Review review);
 }
--- src/main/webapp/html/review/view2.js
+++ src/main/webapp/html/review/view2.js
@@ -9,6 +9,51 @@ var detailNo = getQuerystring('no');
     $('#review-viewCount').val(data.viewCount);
   }));
 
+  $.get('/bitcamp-team-project/app/json/auth/user', function(obj){
+    console.log(obj);
+    if(obj.status == 'fail') {
+      $('#update-btn').hide();
+      $('#delete-btn').hide();
+    } else {
+      $('#update-btn').show();
+      $('#delete-btn').show();
+    }
+});
+  
+  $('#delete-btn').click(() => {
+    $.getJSON('/bitcamp-team-project/app/json/review/delete?no=' + $('#no').val(), 
+            function(data) {
+      //$('#no').val()
+    })
+    .done(function(data) {location.href = "index.html";})
+    .fail(function(data) {
+      console.log(data)
+      alert('삭제 실패입니다!\n' + data.responseText);
+    })
+  });
+
+  $('#update-btn').click(() => {
+    $.post('/bitcamp-team-project/app/json/review/update', {
+      no: $('#no').val(),
+      title: $('#title').val(), 
+      contents: $('#contents').val()
+    }, function() {
+
+    })
+    .done(function() {
+      location.href = "index.html";
+    })
+    .fail(function() {
+      alert('등록 실패입니다!\n' + data.message)
+    })
+  });
+  
+  
+  
+  
+  
+  
+
 
   function getQuerystring(key, default_)
   {
