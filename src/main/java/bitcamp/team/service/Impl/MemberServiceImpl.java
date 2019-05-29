830e64c8a437fd2680db12c33b35a58624dbd885
Leedaegu <dencw6@naver.com>
2019-05-29 20:32:14 +0900

    .

--- src/main/java/bitcamp/team/service/Impl/MemberServiceImpl.java
+++ src/main/java/bitcamp/team/service/Impl/MemberServiceImpl.java
@@ -59,32 +59,12 @@ public class MemberServiceImpl implements MemberService {
   }
 
   @Override
-  public int update(Member member) {
-    return memberDao.update(member);
-  }
-
-  @Override
-  public int delete(int no) {
-    return memberDao.delete(no);
-  }
-
-  @Override
-  public int delete(String email) {
-    return memberDao.deleteCompany(email);
-  }
-
-  @Override
   public Member get(String email, String password) {
     HashMap<String,Object> paramMap = new HashMap<>();
     paramMap.put("email", email);
     paramMap.put("password", password);
 
     return memberDao.findByEmailPassword(paramMap);
-  }
-
-  @Override
-  public int size() {
-    return memberDao.countAll();
   }
 
 }
