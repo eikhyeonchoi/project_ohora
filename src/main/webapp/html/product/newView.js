a8c515bac999c375d8f462bb4f8d3aad9982b144
eikhyeonchoi <eikhyeon.choi@gmail.com>
2019-06-02 21:17:33 +0900

    0602 hyeon product view 수정

--- src/main/webapp/html/product/newView.js
+++ src/main/webapp/html/product/newView.js
@@ -1,86 +1,113 @@
 /**
  * 
  */
-var produdctNo = getQuerystring('no'),
-    type = 0,
-    tipNo = 0;
+var productNo = getQuerystring('no'),
+type = 0,
+tipNo = 0;
 
 var total = 0,
-    satisAver = 0,
-    level = 0,
-    understand = 0,
-    design = 0,
-    asStf = 0,
-    useful = 0,
-    price = 0;
+satisAver = 0,
+level = 0,
+understand = 0,
+design = 0,
+asStf = 0,
+useful = 0,
+price = 0;
+
+var productName = '';
+
+var tipBtn = $('#tip-btn'),
+manualBtn = $('#manual-btn'),
+reviewBtn = $('#review-btn'),
+satisfyBtn = $('#satisfy-btn'),
+productUpdateBtn = $('#product-update-btn'),
+productDeleteBtn = $('#product-delete-btn');
 
 $(document.body).bind('loaded.loginuser', () => {
   window.type = Number(sessionStorage.getItem('type'));
-  $(document).ready(function(){
-    
-    $.get('/bitcamp-team-project/app/json/satisfy/detail?no=' + produdctNo, function(obj) {
-      console.log(obj);
-      for (var el of obj.list) {
-        total += el.asStf + el.design + el.level + el.priceStf + el.understand + el.useful,
-        price += el.priceStf,
-        level += el.level,
-        understand += el.understand,
-        design += el.design,
-        asStf += el.asStf, 
-        useful += el.useful;
-      }
-      satisAver = (total / (obj.totalColumn * 6)).toFixed(2);
-      price = (price / (obj.totalColumn)).toFixed(2);
-      level = (level / (obj.totalColumn)).toFixed(2);
-      understand = (understand / (obj.totalColumn)).toFixed(2);
-      design = (design / (obj.totalColumn)).toFixed(2);
-      asStf = (asStf / (obj.totalColumn)).toFixed(2);
-      useful = (useful / (obj.totalColumn)).toFixed(2);
-      
-      $(document.body).trigger('loaded-satisfy');
-    });
-    
-    
-    
-    $.get('/bitcamp-team-project/app/json/product/detail?no=' + produdctNo, function(obj) {
-      console.log(obj);
-      
-      $('#product-inform-div').prepend('<hr class="head-line">');
-      $('#product-inform-div').prepend('<h5>'+ obj.product.manufacturer.name +'</h5>');
-      $('#product-inform-div').prepend('<h3>'+ obj.product.name +'</h3>');
-      $('#product-inform-div').prepend('<hr class="head-line">');
-      
-      var largeCategory = obj.product.productSmallCategory.productLargeCategory.name;
-      if (largeCategory.includes('태블릿')) {
-        $('#img-div-by-large-category').append("<img src='/bitcamp-team-project/upload/productfile/lc_pad.jpg' style='width: 100%; height: 100%;'>");
-        $('#product-inform-div').prepend('<i class="fas fa-mobile-alt" style="font-size: 5em;"></i>');
-      } else if(largeCategory.includes('카메라')) {
-        $('#img-div-by-large-category').append("<img src='/bitcamp-team-project/upload/productfile/lc_camera.jpg' style='width: 100%; height: 100%;'>");
-        $('#product-inform-div').prepend('<i class="fas fa-camera" style="font-size: 5em;"></i>');
-      } else if(largeCategory.includes('노트북')) {
-        $('#img-div-by-large-category').append("<img src='/bitcamp-team-project/upload/productfile/lc_notebook.png' style='width: 100%; height: 100%;'>");
-        $('#product-inform-div').prepend('<i class="fas fa-laptop" style="font-size: 5em;"></i>');
-      } else if(largeCategory.includes('가전')) {
-        $('#img-div-by-large-category').append("<img src='/bitcamp-team-project/upload/productfile/lc_appliances.jpg' style='width: 100%; height: 100%;'>");
-        $('#product-inform-div').prepend('<i class="fas fa-plug" style="font-size: 5em;"></i>');
-      }
-      
-      // hover 시 썸네일 사진
-      $('#product-thumbnail-img-div').append('<img src="/bitcamp-team-project/upload/productfile/' + obj.product.productFiles[0].img + '_thumb" style="width: 100%; height: auto;">');
-      
-      
-      if (obj.product.tip != null) {
-        window.tipNo = obj.product.tip.no;
-      }
-      
-      $(document.body).trigger({
-        type: 'loaded-product',
-        obj: obj
-      })
-    }); // get
-  }); // ready
+  satisfyBtn.hide();
+  productUpdateBtn.hide();
+  productDeleteBtn.hide();
   
+  if(window.type != 0){
+    satisfyBtn.show();
+  }
+  
+  if(window.type >= 2){
+    productUpdateBtn.show();
+  }
+  
+  if(window.type == 3){
+    productDeleteBtn.show();
+  }
 }); // bind loaded.loginuser
+
+
+
+$(document).ready(function(){
+  $.get('/bitcamp-team-project/app/json/satisfy/detail?no=' + productNo, function(obj) {
+    console.log(obj);
+    window.productName = obj.list[0].product.name;
+    for (var el of obj.list) {
+      total += el.asStf + el.design + el.level + el.priceStf + el.understand + el.useful,
+      price += el.priceStf,
+      level += el.level,
+      understand += el.understand,
+      design += el.design,
+      asStf += el.asStf, 
+      useful += el.useful;
+    }
+    satisAver = (total / (obj.totalColumn * 6)).toFixed(2);
+    price = (price / (obj.totalColumn)).toFixed(2);
+    level = (level / (obj.totalColumn)).toFixed(2);
+    understand = (understand / (obj.totalColumn)).toFixed(2);
+    design = (design / (obj.totalColumn)).toFixed(2);
+    asStf = (asStf / (obj.totalColumn)).toFixed(2);
+    useful = (useful / (obj.totalColumn)).toFixed(2);
+
+    $(document.body).trigger('loaded-satisfy');
+  });
+
+
+  $.get('/bitcamp-team-project/app/json/product/detail?no=' + productNo, function(obj) {
+    console.log(obj);
+
+    $('#product-inform-div').prepend('<hr class="head-line">');
+    $('#product-inform-div').prepend('<h5>'+ obj.product.manufacturer.name +'</h5>');
+    $('#product-inform-div').prepend('<h3>'+ obj.product.name +'</h3>');
+    $('#product-inform-div').prepend('<hr class="head-line">');
+
+    var largeCategory = obj.product.productSmallCategory.productLargeCategory.name;
+    if (largeCategory.includes('태블릿')) {
+      $('#img-div-by-large-category').append("<img src='/bitcamp-team-project/upload/productfile/lc_pad.jpg' style='width: 100%; height: 100%;'>");
+      $('#product-inform-div').prepend('<i class="fas fa-mobile-alt" style="font-size: 5em;"></i>');
+    } else if(largeCategory.includes('카메라')) {
+      $('#img-div-by-large-category').append("<img src='/bitcamp-team-project/upload/productfile/lc_camera.jpg' style='width: 100%; height: 100%;'>");
+      $('#product-inform-div').prepend('<i class="fas fa-camera" style="font-size: 5em;"></i>');
+    } else if(largeCategory.includes('노트북')) {
+      $('#img-div-by-large-category').append("<img src='/bitcamp-team-project/upload/productfile/lc_notebook.png' style='width: 100%; height: 100%;'>");
+      $('#product-inform-div').prepend('<i class="fas fa-laptop" style="font-size: 5em;"></i>');
+    } else if(largeCategory.includes('가전')) {
+      $('#img-div-by-large-category').append("<img src='/bitcamp-team-project/upload/productfile/lc_appliances.jpg' style='width: 100%; height: 100%;'>");
+      $('#product-inform-div').prepend('<i class="fas fa-plug" style="font-size: 5em;"></i>');
+    }
+
+    // hover 시 썸네일 사진
+    $('#product-thumbnail-img-div').append('<img src="/bitcamp-team-project/upload/productfile/' + obj.product.productFiles[0].img + '_thumb" style="width: 100%; height: auto;">');
+
+
+    if (obj.product.tip != null) {
+      window.tipNo = obj.product.tip.no;
+    }
+
+    $(document.body).trigger({
+      type: 'loaded-product',
+      obj: obj
+    })
+  }); // get
+}); // ready
+
+
 
 
 $(document.body).bind('loaded-product', function(data){
@@ -88,35 +115,114 @@ $(document.body).bind('loaded-product', function(data){
     $(this).css('background-color', 'white');
     $(this).children().eq(0).css('display', 'none');
     $(this).children().eq(1).css('display', '');
-    
+
   }).mouseleave(function() {
     $(this).css('background-color', 'black');
     $(this).children().eq(0).css('display', '');
     $(this).children().eq(1).css('display', 'none');
-    
   });
-}) // bind loaded-essential
+
+  reviewBtn.click(function() {
+    location.href = '../review/view.html?no=' + productNo + '&name=' + productName;
+  })
+
+  satisfyBtn.click(function() {
+    $.get('/bitcamp-team-project/app/json/product/findReviewedMember?pNo=' + productNo, function(obj) {
+      if (obj.status == 'fail') {
+        swal("만족도 평가 오류", "이미 만족도를 등록하셨습니다", "warning");
+        $('#go-satisfy-add-btn').prop('disabled',true);
+      } else {
+        location.href = '../satisfy/add.html?productNo=' + productNo;
+      }
+    }) // get
+  }) // click
+
+
+  tipBtn.click(function() {
+    $.getJSON('/bitcamp-team-project/app/json/product/confirmTip?no=' + productNo, function(obj){
+      if (obj.tipCount == 1){
+        location.href = '../tip/view.html?no=' + productNo;
+      } else {
+        if (window.type < 1){
+          alert('이 제품의 대한 팁이 존재하지 않습니다');
+        } else {
+          swal("팁 보기 오류", "등록된 팁이 존재하지 않습니다\n 팁을 등록해주세요", "info");
+          tipBtn.text('팁등록');
+          tipBtn.click(function() {
+            location.href = '/bitcamp-team-project/html/tip/form.html?no=' + productNo;
+          });
+        }
+      }
+    });
+  });
+
+  productUpdateBtn.click(function() {
+    location.href = 'update.html?no=' + productNo;
+  })
+
+  productDeleteBtn.click(function() {
+    $.get('/bitcamp-team-project/app/json/product/delete?no=' + productNo + '&tipNo=' + window.tipNo, function(obj){
+      if (obj.status == 'success') {
+        location.href = 'index.html';
+
+      } else {
+        alert('삭제 실패!! \n' + obj.message);
+      }
+    })
+  });
+
+}) // bind loaded-product
 
 $(document.body).bind('loaded-satisfy', function(){
-  
-  if (isValidScore(satisAver) == true) { 
-    generatePieChart('pie-canvas', satisAver);
-  } else {
-    $('#chart-div').append('<h4>아직 등록된 만족도 정보가 없습니다</h4>');
-    generatePieChart('pie-canvas', 0);
+  var obj = {
+      name01: '가격 만족도',
+      name02: '사용 난이도',
+      name03: '이해도',
+      name04: '디자인 만족도',
+      name05: 'A/S 만족도',
+      name06: '사용 만족도',
+      score01: price,
+      score02: level,
+      score03: understand,
+      score04: design,
+      score05: asStf,
+      score06: useful
   }
-  
-  
+
+  if (isValidScore(satisAver) == true) { 
+    $('#user-evaluation-div').append('<h3>' + window.productName + ' 의 대한 사용자들의</h3>');
+    $('#user-evaluation-div').append('<h3>평균만족도는 ?</h3>');
+    if (satisAver >= 4.00) {
+      $('#user-evaluation-div').append('<i class="far fa-smile-beam" style="font-size: 5em; color: green;"></i>');
+      $('#user-evaluation-div').append('<h3>good</h3>');
+    } else if (satisAver >= 2.00 ||satisAver < 4.00) {
+      $('#user-evaluation-div').append('<i class="far fa-meh" style="font-size: 5em; color: black;"></i>');
+      $('#user-evaluation-div').append('<h3>soso</h3>');
+    } else {
+      $('#user-evaluation-div').append('<i class="far fa-frown" style="font-size: 5em; color: red;"></i>');
+      $('#user-evaluation-div').append('<h3>bad</h3>');
+    }
+    generatePieChart('pie-canvas', satisAver);
+    generateBarChart('bar-canvas', obj);
+  } else {
+    $('#user-evaluation-div').append('<h3>아직 등록된 만족도 정보가 없습니다</h3>');
+    generatePieChart('pie-canvas', 0);
+    generateBarChart('bar-canvas', obj);
+  }
+
+
   $("#fixed-background-div").mouseenter(function() {
-    //$(this).css('background-color', 'white');
-    //$(this).children().eq(0).css('display', 'none');
-    //$(this).children().eq(1).css('display', '');
-    
+    $(this).children().eq(0).css('display', 'none');
+    $(this).children().eq(1).css('display', '');
   }).mouseleave(function() {
-    //$(this).children().eq(0).css('display', '');
-    //$(this).children().eq(1).css('display', 'none');
+    $(this).children().eq(0).css('display', '');
+    $(this).children().eq(1).css('display', 'none');
+
   });  
 });
+
+
+
 
 
 function isValidScore(value) { 
@@ -126,6 +232,44 @@ function isValidScore(value) {
     return true;
   }
 } // checkScore
+
+
+function generateBarChart(id, obj) {
+  new Chart($('#' + id), {
+    type: "bar",
+    data: {
+      labels: [obj.name01, obj.name02, obj.name03, obj.name04, obj.name05, obj.name06],
+      datasets: [{
+        label: window.productName + '의 대한 만족도',
+        data: [obj.score01, obj.score02, obj.score03, obj.score04, obj.score05, obj.score06],
+        fill: false,
+        backgroundColor: [
+          "rgba(77, 86, 86, 0.7)",
+          "rgba(21, 67, 96, 0.7)",
+          "rgba(123, 125, 125, 0.7)",
+          "rgba(11, 83, 69, 0.7)",
+          "rgba(120, 66, 18, 0.7)",
+          "rgba(23, 32, 42, 0.7)"
+          ]
+      }]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          barThickness: 40,
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 5,
+            stepSize: 0.5,
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  }); // chart
+}
 
 
 function generatePieChart(id, value){
@@ -168,7 +312,6 @@ function getQuerystring(key, default_){
   else
     return qs[1];
 } // getQuerystring
-
 
 
 //도넛 차트에서 필요한 plugin
