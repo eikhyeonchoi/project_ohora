0ebc1715a39b0ec96172b733cb5e51d679d0e9b4
EuiJoo.Kim <shopingid@naver.com>
2019-06-12 16:38:32 +0900

    EJoo-review; 리뷰 많은 순 추가!

--- src/main/java/bitcamp/team/domain/Product.java
+++ src/main/java/bitcamp/team/domain/Product.java
@@ -15,11 +15,21 @@ public class Product {
   private Tip tip;
   private Review review;
 
+  private int reviewCount;
+
   @Override
   public String toString() {
     return "Product [no=" + no + ", smallCategoryNo=" + smallCategoryNo + ", manufacturerNo="
         + manufacturerNo + ", name=" + name + ", manufacturer=" + manufacturer
         + ", productSmallCategory=" + productSmallCategory + ", productFiles=" + productFiles + "]";
+  }
+
+  public int getReviewCount() {
+    return reviewCount;
+  }
+
+  public void setReviewCount(int reviewCount) {
+    this.reviewCount = reviewCount;
   }
 
   public Review getReview() {
