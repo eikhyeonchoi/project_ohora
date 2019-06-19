6d733dfe9dd250c6378ed550e15e272026bdc7ef
Leedaegu <dencw6@gmail.com>
2019-06-19 19:08:10 +0900

    ,

--- src/main/webapp/html/myPage/leaveId.js
+++ src/main/webapp/html/myPage/leaveId.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-	$("#leave-ck").change(function(){
-		if($("#leave-ck").is(":checked")){
-			$('#leave-btn').removeAttr("disabled");
-		}else{
-			$('#leave-btn').attr("disabled","true");
-		}
-	});
 
-	$("#leave-btn").click(function() {
-		location.replace("leaveId2.html");
-	})
+  $(document.body).bind('loaded.loginuser', function() {
+    var email = sessionStorage.getItem('email');
+    $('#my-email').html(email);
+  });
+  
+  $("#leave-ck").change(function(){
+    if($("#leave-ck").is(":checked")){
+      $('#leave-btn').removeAttr("disabled");
+    }else{
+      $('#leave-btn').attr("disabled","true");
+    }
+  });
+
+  $("#leave-btn").click(function() {
+    location.replace("leaveId2.html");
+  })
 });
